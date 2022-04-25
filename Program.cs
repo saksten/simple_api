489bc39610a2946aa8b2e11e02b3b665c6c02e2f
Sakarias Stenbäck <sakarias.stenback@gmail.com>
2022-04-25 18:30:52 +0300

    test commit

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// testing git
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
